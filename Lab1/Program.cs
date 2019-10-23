2eaa64ca266964b63a05bb365e263605ff8bff28
Indiacam01 <54690577+Indiacam01@users.noreply.github.com>
2019-10-23 19:42:29 -0400

    Add files via upload

---
+++ Lab1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Week1IndiaCampbell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is from Cleveland!");
+
+            int num1, num2, num3;
+
+            num1 = 27;
+            num2 = 55;
+
+            num3 = num1 * num2;
+
+            Console.WriteLine("Product of " + "num1" + " and " + num2 + " is " + num3);
+
+
+
+            string str1, str2;
+            str1 = "Hello World";
+            str2 = "India Campbell";
+            Console.WriteLine(str1 + " from " str2);
+            
+
+       
+
+        }
+    }
+}
