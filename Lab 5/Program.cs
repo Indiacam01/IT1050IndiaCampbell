4c25011779a7bf7b31aafaae9560ec4ba6a6f33b
Indiacam01 <54690577+Indiacam01@users.noreply.github.com>
2019-11-23 20:33:45 -0500

    Update Program.cs

--- Lab 5/Program.cs
+++ Lab 5/Program.cs
@@ -1,1 +1,16 @@
-m
+using System;
+
+namespace IT1050IndiaCampbellLab 5
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+         class MyDog
+    {
+        MyDog = new Dog("morris");
+    }
+
+        MyDog.bark("morris");
+    
+    }
