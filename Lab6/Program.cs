156ddaf4a6984c89297f52da0cf6053cf1521485
Indiacam01 <54690577+Indiacam01@users.noreply.github.com>
2019-11-30 16:28:08 -0500

    Update Program.cs

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -1,1 +1,16 @@
-z
+using System;
+
+namespace IT1050IndiaCampbellLab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[]months = {"January","Febuary", "March", "April", "May","June", "July","August", "September", "October", "November", "December"};
+
+            for (int i = 0; i < months.Length; i++) 
+               Console.WriteLine("{0} {1}", i + 1, months[i]);
+        }
+    }
+}
+
